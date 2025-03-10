4620543ed23670e11a8737a24752f2958ab74edd
Nuria Gómez-Vargas <ngvargas@us.es>
2025-03-10 17:28:27 +0100

    Add files via upload

---
+++ formulation_fUNknown.py
@@ -0,0 +1,172 @@
+import gurobipy as gp
+import numpy as np
+import time
+
+
+def problem_fUNknown(predef_W, 
+                     N, L, dataset, A, list_bn, groups_N, considered_groups, 
+                     timelimit=None, silence = False, linearize_product=False,
+                     predef_C = None, known_objs = None, lambd = 1.,
+                     predef_u = None, predef_X = None):
+
+    _,d = dataset.shape
+    m,_ = A.shape
+    
+    L_upto = L
+    L,K = predef_W.shape
+    
+    # List to store feasible solutions
+    solution_log = []
+    count_checks = 0
+    progress_logs = [(0., float('inf'), 0.)]
+    saved_best_time, saved_best_objval, saved_best_bound = None, float('inf'), 0.
+    def my_callback(model, where):
+
+        global count_checks
+
+        if where == gp.GRB.Callback.MIPSOL:  # When a new feasible solution is found
+
+            obj_val = model.cbGet(gp.GRB.Callback.MIPSOL_OBJ)  # Objective value
+            runtime = model.cbGet(gp.GRB.Callback.RUNTIME)  # Time when found
+            gap = model.cbGet(gp.GRB.Callback.MIPSOL_OBJBST)  # Best bound so far
+            best_bound = model.cbGet(gp.GRB.Callback.MIPSOL_OBJBND)  # Current best bound
+            mip_gap = abs(best_bound - obj_val) / abs(obj_val) if obj_val != 0 else float('inf')  # Compute MIP gap
+            
+            # Extract variable values
+            var_values = {var.VarName: model.cbGetSolution(var) for var in model.getVars()}
+            
+            # Print information dynamically
+            #var_str = ", ".join([f"{k}={v:.4f}" for k, v in var_values.items() if "W" in k])
+            #print(f"Feasible Solution Found - Time: {runtime:.2f}s, Objective: {obj_val:.4f}, Gap: {mip_gap:.4%}, Solution: {var_str}")
+            C_feas = np.array([v for k, v in var_values.items() if "C" in k]).reshape((K,d))
+            X_feas = np.array([v for k, v in var_values.items() if "X" in k]).reshape((N,L))
+            print(f"Feasible Solution Found - Time: {runtime:.2f}s, Objective: {obj_val:.4f}, Gap: {mip_gap:.4%}, \nSolution C_feas: \n{C_feas.round(3)}\n Cluster composition: {X_feas.sum(axis=0)}")
+            
+            # Store in a structured format
+            solution_log.append({"Time (s)": runtime, 
+                                 "Objective": obj_val, 
+                                 "Gap (%)": mip_gap * 100,
+                                 "C_feas": C_feas,
+                                 "X_feas": X_feas,
+                                 **var_values  # Merge variable values into the dictionary
+                                 })
+            
+        elif where == gp.GRB.Callback.MIP: # General MIP progress
+
+            runtime = model.cbGet(gp.GRB.Callback.RUNTIME)
+            if runtime - progress_logs[-1][0] >5:
+                #print("ENTRO EN SEGUNDO BUCLE COMPROBACION: ", runtime - progress_logs[-1][0])
+                best_sol = model.cbGet(gp.GRB.Callback.MIP_OBJBST)  # Best found feasible solution
+                best_bound = model.cbGet(gp.GRB.Callback.MIP_OBJBND)  # Current best bound
+                
+                # Stop criteria
+                if best_sol < progress_logs[-1][1]:
+                    #print("improving obj val")
+                    count_checks = 0
+                elif best_bound > progress_logs[-1][2]:
+                    #print("improving bound")
+                    count_checks = 0
+                else:
+                    count_checks +=1
+                    #print("not improving")
+                progress_logs.append((runtime,best_sol,best_bound))
+                
+                if count_checks > 100 :
+                    print("STOPPING BECAUSE THERE IS NO IMPROVEMENT")
+                    model.terminate()  # Force stop Gurobi
+
+
+    model = gp.Model()
+
+    if silence:
+        model.setParam('OutputFlag', 0)
+    
+    if predef_C is None:
+        C = model.addMVar(shape=(K,d), lb=-1, ub=1., vtype=gp.GRB.CONTINUOUS, name="C")
+        c0 = model.addMVar(shape=K, 
+                           lb=-float('inf'), ub=+float('inf'),
+                           vtype=gp.GRB.CONTINUOUS, name="c0")
+        S = model.addMVar(shape=(K,d),
+                          vtype=gp.GRB.BINARY, name="S")
+        P = model.addMVar(shape=(K,d),
+                          lb=0., ub=1.,
+                          vtype=gp.GRB.CONTINUOUS, name="P")
+        model.addConstrs((2*P[k,j] - C[k,j] >= 0 
+                          for k in range(K) for j in range(d)), name="define_absval")
+        model.addConstrs((gp.quicksum(2*P[k,j] - C[k,j] for j in range(d)) == 1
+                          for k in range(K)), name="normalization")
+
+        model.addConstrs((P[k,j] <= S[k,j]
+                        for k in range(K) for j in range(d)), name="define_P1")
+        model.addConstrs((P[k,j] <=  C[k,j] + (1-S[k,j])
+                        for k in range(K) for j in range(d)), name="define_P2")
+        model.addConstrs((P[k,j] >=  C[k,j] - (1-S[k,j])
+                        for k in range(K) for j in range(d)), name="define_P3")
+    else:
+        C = predef_C
+
+    if predef_X is None:
+        X = model.addMVar(shape=(N,L), vtype=gp.GRB.BINARY, name="X")
+        model.addConstrs((gp.quicksum(X[n,l] for l in range(L)) == 1
+                          for n in range(N)),
+                          name="all instances belong only to one cluster")
+    else:
+        X = predef_X
+    
+    W = predef_W
+    if L > L_upto:
+        #INTERPRETABILITY FROM CATALOGUE 
+        y = model.addMVar(shape=(L), vtype=gp.GRB.BINARY, name="Y")
+        model.addConstrs((y[l] >= X[n,l] for n in range(N) for l in range(L)), name="catalogue_active")
+        model.addConstr(gp.quicksum(y[l] for l in range(L))<=L_upto, name="chosen_from_catalogue")
+
+    different_Z_cal = len(considered_groups)
+    if predef_u is None:
+        u = model.addMVar(shape=(different_Z_cal,L,m),
+                          lb=-float('inf'), ub=0., vtype=gp.GRB.CONTINUOUS, name="u")
+        model.addConstrs((u[feas_reg,l,:]@A == W[l,:]@C
+                    for feas_reg in range(different_Z_cal) for l in range(L)), name="dual_constr")
+    else:
+        u = predef_u
+       
+    #COMPUTATIONAL ENHANCENMENT ?
+    if linearize_product:
+        v = model.addMVar(shape=(N,L,m),
+                        lb=-float('inf'), ub=0., vtype=gp.GRB.CONTINUOUS, name="v")
+        model.addConstrs((v[n,l,i] == X[n,l] * u[considered_groups.index(groups_N[n]),l,i]
+                            for n in range(N) for l in range(L) for i in range(m)), name="realocate_product")
+    
+    def term(l,n):
+        if linearize_product:
+            return v[n,l,:] @ (A@dataset[n,:] - list_bn[n])
+        else:
+            return X[n,l] * u[considered_groups.index(groups_N[n]),l,:] @ (A@dataset[n,:] - list_bn[n])
+        
+    def linreg(known_objs):
+        if known_objs is None:
+            return 0
+        else:
+            return gp.quicksum( (known_objs[n][k] - C[k,:]@dataset[n,:])**2#(known_objs[n][k] - c0[k] - C[k,:]@dataset[n,:])**2
+                               for n in known_objs.keys() for k in range(K))
+
+    model.setObjective(lambd * gp.quicksum(term(l,n) for l in range(L) for n in range(N)) + (1-lambd)*linreg(known_objs),
+                       sense=gp.GRB.MINIMIZE)
+    #model.addConstrs((term(l,n) >= 0 for l in range(L) for n in range(N)))
+    model.addConstr(linreg(known_objs) >= 0)
+    
+
+    model.update()
+
+    if timelimit is not None:
+        model.setParam(gp.GRB.Param.TimeLimit, timelimit)
+    
+    model.optimize(my_callback)
+    """
+    if (predef_W is None) and (predef_X is None):
+        model.optimize(my_callback)
+    else:
+        model.optimize()
+    """
+    
+    
+    return model, C,c0, X, u, solution_log, progress_logs
---
+++ main_fUNknown.py
@@ -0,0 +1,286 @@
+from formulation_fUNknown import problem_fUNknown
+import group_diet_instances as diet
+import numpy as np
+import time
+import pandas as pd
+import pickle
+import auxfuncs as f
+import math
+import os
+import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+#import imageio
+from heuristic_fknown import cluster_assignments
+from sklearn.metrics.pairwise import cosine_similarity
+                                    
+
+# Permanently changes the pandas settings
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+# Set print options to suppress scientific notation
+np.set_printoptions(suppress=True)
+
+#parameters
+N_test= 100
+list_N = [25]
+list_K = [2,3]
+list_d = [10]
+list_lambd = [0.1,0.2,0.25,0.5]
+list_n_vars_perturbed = [0,1] #<= d, number of variables
+timelimit = 3600.
+
+
+for seed in [0]:
+    for N in list_N:
+        for K in list_K:
+            for d in list_d:
+                for n_vars_perturbed in list_n_vars_perturbed:
+                    
+                    ###################################################
+                    #diet problem
+                    print("DATA CONFIGURATION: (seed, N, K, d, n_vars_perturbed)=", (seed,N,K,d, n_vars_perturbed))
+                    instances = diet.get_diet_instances(100+N_test, K, d, n_vars_perturbed, seed)
+                    data, chosen_foods, list_of_objectives, list_of_constraints, constraints, considered_groups, C_orig, W_orig, A, groups_N, list_bn, dataset = instances
+                    
+                    W_orig_train, dataset_train, list_bn_train, groups_N_train = W_orig[:N,:], dataset[:N,:], list_bn[:N], groups_N[:N]
+                    W_orig_test, dataset_test, list_bn_test, groups_N_test = W_orig[-N_test:,:], dataset[-N_test:,:], list_bn[-N_test:], groups_N[-N_test:]
+                    
+                    #number of clusters
+                    if K ==2:
+                        L_max = K +1
+                    if K>2:
+                        L_max = K + math.comb(K, math.ceil(K/2))
+                    list_L = list(range(2,L_max+1))
+                    
+                    for L in list_L:
+                        for lambd in list_lambd:
+                        
+                            print("\n-------------\n")
+                            print("NUMBER OF CLUSTERS REQUIRED L =", L)
+                            
+                            name = str(("seed",seed,"N",N,"K",K,"d",d,"n_pert",n_vars_perturbed,"L",L))
+
+                            ###################################################
+                            
+                            #CHECK IF ALREADY DONE
+                            already_done = np.any([('soloutput-fUNKNOWN-'+str(lambd)+'-'+name) in string for string in os.listdir(r"./collecting outputs")])
+                            if not already_done:
+                                print("NOT ALREADY DONE")
+
+                                known_objs = {}
+                                #here, play with N
+                                for n in range(N):
+                                    known_objs[n] = {}
+                                    for k in range(K):
+                                        known_objs[n][k] = C_orig[k,:]@dataset[n,:]
+
+                                if K==2:
+                                    predef_W = np.array([[1., 0.],
+                                                        [0., 1.],
+                                                        [.25, .75],
+                                                        [.75, .25],
+                                                        [.5, .5]])
+                                elif K==3:
+                                    predef_W = np.array([[1., 0., 0.],
+                                                        [0., 1., 0.],
+                                                        [0., 0., 1.],
+                                                        [.5, .5, 0.],
+                                                        [.25, .75, 0.],
+                                                        [.75, .25, 0.],
+                                                        [.5, 0., .5],
+                                                        [.25, 0., .75],
+                                                        [.75, 0., .25],
+                                                        [0., .5, .5],
+                                                        [0., .25, .75],
+                                                        [0., .75, .25],
+                                                        [1/3, 1/3, 1/3]])
+                                    
+                                inicio = time.time()
+                                output  = problem_fUNknown(predef_W, 
+                                                           N, L, dataset_train, A, list_bn_train,  groups_N_train, considered_groups,
+                                                           timelimit=timelimit, 
+                                                           known_objs =  known_objs, lambd=lambd)
+                                time_sol = time.time() - inicio
+                                
+                                model, C,c0, X, u, _, _ = output
+                                W=predef_W
+                                
+                                print("time to sol: ", round(time_sol,2), "s.")
+                                gap = 0. if model.status == 2 else model.MIPGap*100
+                                obj_val_solver = model.objVal
+                                print("objective val from solver: ", obj_val_solver, ", gap: ", gap)
+                                
+                                try:
+                                    W_re, X_re, L_efective, delete_cluster = f.regroup(W.X,X.X)
+                                except:
+                                    W_re, X_re, L_efective, delete_cluster = f.regroup(W,X.X) #used predef_W != None for interpretability
+                                print("W:\n", np.round(W_re,3))
+                                #print("X:\n", X_re.astype(int))
+                                print("cluster composition, sum X by columns: ", X_re.sum(axis = 0))
+                                print("efective number of clusters: ", L_efective)
+
+                                print("ESTIMATED COSTS: ")
+                                print(C.X.round(2))
+                                print("WITH INTERCEPTS: ", c0.X.round(2))
+
+                                print("ORIGINAL COSTS: ")
+                                print(C_orig.round(2))
+
+
+                                sims = []
+                                for k in range(K):
+                                    sims.append( cosine_similarity(np.array(C_orig[k,:]).reshape(1, -1), np.array(C.X[k,:]).reshape(1, -1))[0][0] )
+                                mean_sim_obj = np.mean(sims)
+                                print("COSINE SIMILARITIES IN OBJS: ", sims, " WITH MEAN=",mean_sim_obj)
+                                """
+                                #depict clusters
+                                if K in [2,3]:
+                                    colors = ["b","g","r","m","y","k","c"]
+                                    clust = dict([(l,colors.pop()) for l in range(7)])
+                                    if K==2:
+                                        fig, ax = plt.subplots()
+                                    if K==3:
+                                        fig = plt.figure()
+                                        ax = fig.add_subplot(projection='3d')
+                                        ax.view_init(elev=30, azim=50)
+                                    fig.suptitle(formulation)
+                                    ax.set_xlim((0,1))
+                                    ax.set_ylim((0,1))
+                                    if K==3:
+                                        ax.set_zlim((0,1))
+                                    for n in range(N):
+                                        if K==2:
+                                            ax.scatter(W_orig[n,0], W_orig[n,1], color=clust[X_re[n,:].argmax()])
+                                        if K==3:
+                                            ax.scatter(W_orig[n,0], W_orig[n,1], W_orig[n,2], color=clust[X_re[n,:].argmax()])
+                                    for l in range(W_re.shape[0]):
+                                        w_l = W_re[l,:]
+                                        if w_l.sum()!= 0.:
+                                            if K==2:
+                                                ax.scatter(w_l[0],w_l[1], s=75,marker="X", color=clust[l],label="cluster_"+str(l+1))
+                                            if K ==3:
+                                                ax.scatter(w_l[0],w_l[1],w_l[2], s=75,marker="X", color=clust[l],label="cluster_"+str(l+1))
+                                    fig.legend()
+                                    
+                                    # Save rotation frames
+                                    #filenames = []
+                                    #for angle in range(0, 360, 10):  # Rotate every 10 degrees
+                                        #   ax.view_init(elev=30, azim=angle)
+                                        #  filename = f"frame_{angle}.png"
+                                        # plt.savefig(filename)
+                                        #filenames.append(filename)
+
+                                    # Create GIF
+                                    #images = [imageio.imread(f) for f in filenames]
+                                    #imageio.mimsave('3d_plot_rotation.gif', images, duration=0.1)
+                                    
+                                    plt.savefig(name+"-"+formulation+"-TRAINscatter.svg")
+                                    plt.close()
+                                    #plt.show()
+                                """
+                                
+                                ####################
+                                TRAIN_metrics = zip(*[f.analyze_instance(W_orig_train[n,:],
+                                                                        W_re[X_re[n,:].argmax(),:]) 
+                                                        for n in range(N)])
+                                TRAIN_equal_maxs, TRAIN_rmse, TRAIN_rho, TRAIN_tau, TRAIN_cos_sim, TRAIN_emd = TRAIN_metrics
+                                TRmean_equal_maxs, TRmean_rmse, TRmean_rho, TRmean_tau, TRmean_cos_sim, TRmean_emd = [np.mean(m).round(2) for m in [TRAIN_equal_maxs, TRAIN_rmse, TRAIN_rho, TRAIN_tau, TRAIN_cos_sim, TRAIN_emd]]
+                                print("MEAN TRAIN performance metrics: (equal_maxs, rmse, rho, tau, cos_sim, emd): ",
+                                    TRmean_equal_maxs, TRmean_rmse, TRmean_rho, TRmean_tau, TRmean_cos_sim, TRmean_emd)
+
+                                
+                                #EVALUATING OUT SAMPLE
+                                X_test, _ = cluster_assignments(u.X, dataset_test, A, list_bn_test, groups_N_test, considered_groups,
+                                                                delete_cluster = delete_cluster)
+                                clusters_test = [np.argmax(x) for x in X_test]
+                                
+                                TEST_metrics = zip(*[f.analyze_instance(W_orig_test[n,:], 
+                                                                        W_re[clusters_test[n],:]) 
+                                                        for n in range(N_test)])
+                                TEST_equal_maxs, TEST_rmse, TEST_rho, TEST_tau, TEST_cos_sim, TEST_emd = TEST_metrics
+                                TSmean_equal_maxs, TSmean_rmse, TSmean_rho, TSmean_tau, TSmean_cos_sim, TSmean_emd = [np.mean(m).round(2) for m in [TEST_equal_maxs, TEST_rmse, TEST_rho, TEST_tau, TEST_cos_sim, TEST_emd]]
+                                print("MEAN TEST performance metrics: (equal_maxs, rmse, rho, tau, cos_sim, emd): ",
+                                    TSmean_equal_maxs, TSmean_rmse, TSmean_rho, TSmean_tau, TSmean_cos_sim, TSmean_emd)
+                                
+                                """
+                                #real vs. estimated instability
+                                comparison_instabs = {'real': [], 'estimated': [], 'dif': []}
+                                for n in range(N):
+                                    W_s = {'estimated':W_re[X_re[n,:].argmax(),:], 'real': W_orig_train[n,:]}
+                                    for vect in ['estimated','real']:
+                                        W_chosen = W_s[vect]
+                                        linear_obj = W_chosen@C
+                                        inst = linear_obj@(dataset_train[n,:] - diet.original_problem(linear_obj, A, list_bn_train[n], 0))
+                                        comparison_instabs[vect].append(inst)
+                                    comparison_instabs['dif'].append( comparison_instabs['real'][-1] - comparison_instabs['estimated'][-1])
+                                print("real instab - estimated instab: (mean,std) ", (np.mean(comparison_instabs['dif']),np.std(comparison_instabs['dif'])))
+                                
+                                TEST_comparison_instabs = {'real': [], 'estimated': [], 'dif': []}
+                                for n in range(N_test):
+                                    W_s = {'estimated':W_re[clusters_test[n],:], 'real': W_orig_test[n,:]}
+                                    for vect in ['estimated','real']:
+                                        W_chosen = W_s[vect]
+                                        linear_obj = W_chosen@C
+                                        inst = linear_obj@(dataset_test[n,:] - diet.original_problem(linear_obj, A, list_bn_test[n], 0))
+                                        TEST_comparison_instabs[vect].append(inst)
+                                    TEST_comparison_instabs['dif'].append( TEST_comparison_instabs['real'][-1] - TEST_comparison_instabs['estimated'][-1])
+                                print("real instab - estimated instab: (mean,std) ", (np.mean(TEST_comparison_instabs['dif']),np.std(TEST_comparison_instabs['dif'])))
+                                """
+
+                                """
+                                fig1, ax1 = plt.subplots()
+                                ax1.set_title('TEST COMPARISON INSTABS')
+                                ax1.boxplot(TEST_comparison_instabs['dif'])
+                                plt.savefig(name+"-"+formulation+"-TESTboxplot.svg")
+                                plt.close()
+                                #plt.show()
+                                """
+
+                                sol_outputs = {'seed': seed,
+                                            'N': N,
+                                            'n_vars_perturbed': n_vars_perturbed,
+                                            'instances': instances,
+                                            'K': K,
+                                            'd': d,
+                                            'L': L, 
+                                            'lambda': lambd,
+                                            'optimality': model.status == 2,
+                                            'time': round(time_sol,3),
+                                            'gap': gap,
+                                            #'solution_log': solution_log, 
+                                            #'progress_loss': progress_loss,
+                                            'obj_val': obj_val_solver,
+                                            'print_W': str(W_re.round(2)),
+                                            'W': np.round(W_re,3),
+                                            "C": np.round(C.X, 2),
+                                            "c0": np.round(c0.X, 2),
+                                            'X': X_re.astype(int),
+                                            'X_composition': X_re.sum(axis = 0),
+                                            'L_efective': L_efective,
+                                            'delete_clusters': delete_cluster,
+                                            'clusters_test': clusters_test,
+                                            'mean_sim_obj': mean_sim_obj,
+                                            'sims': sims,
+                                            #'TRcomparison_instabs': comparison_instabs,
+                                            #'TRcomparison_instabs_mean': np.mean(comparison_instabs['dif']),
+                                            'TRAIN_metrics': TRAIN_metrics,
+                                            'TRmean_equal_maxs': TRmean_equal_maxs,
+                                            'TRmean_rmse': TRmean_rmse,
+                                            'TRmean_rho': TRmean_rho,
+                                            'TRmean_tau': TRmean_tau,
+                                            'TRmean_cos_sim': TRmean_cos_sim,
+                                            'TRmean_emd': TRmean_emd,
+                                            #'TScomparison_instabs': TEST_comparison_instabs,
+                                            #'TScomparison_instabs_mean': np.mean(TEST_comparison_instabs['dif']),
+                                            'TEST_metrics': TEST_metrics,
+                                            'TSmean_equal_maxs': TSmean_equal_maxs,
+                                            'TSmean_rmse': TSmean_rmse,
+                                            'TSmean_rho': TSmean_rho,
+                                            'TSmean_tau': TSmean_tau,
+                                            'TSmean_cos_sim': TSmean_cos_sim,
+                                            'TSmean_emd': TSmean_emd
+                                            }
+
+                                with open('soloutput-fUNKNOWN-'+str(lambd)+'-'+name+'.pkl', 'wb') as handle:
+                                    pickle.dump(sol_outputs, handle, protocol=pickle.HIGHEST_PROTOCOL)
